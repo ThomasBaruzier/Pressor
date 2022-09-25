fb7a88a0c68cb4eeafa9b77650e8dece8087cbe4
TyraVex <thomas.baruzier@gmail.com>
2022-09-25 21:21:10 +0200

    update

--- rewrite-pressor.sh
+++ rewrite-pressor.sh
@@ -21,15 +21,15 @@ menu() {
     blank="$(((COLUMNS-30)/2))"
     for ((i=0; i < "$COLUMNS"; i++)); do border+=':'; done
     echo -e "\e[36m\e[3;0f${border}\e[$((LINES-1));0f${border}\e[0m\e[1;${blank}f   ___\n\e[2;${blank}f  / _ \_______ ___ ___ ___  ____\n\e[3;$((blank+1))f/ ___/ __/ -_|_-<(_-</ _ \/ __/\n\e[4;${blank}f/_/  /_/  \__/___/___/\___/_/"
-    echo -e "\e[$(((LINES/2-7)))B"
+    echo -e "\e[$(((LINES/2-8)))B"
   else
     echo -e "\e[$(((LINES/2)-4))B"
   fi
 
   # display menu
-  echo -e "\e[$(((COLUMNS-${#1}-4)/2))C\e[32m> ${1} <\e[0m\e[B"
+  echo -e "  [\e[32m*\e[0m] ${1}\n"
   for i in "${@:2}"; do
-    echo -e "\e[$(((COLUMNS-${#i}-4)/2))C  ${i}  \e[B"
+    echo -e "  [ ] ${i}\n"
   done
 
   while true; do
@@ -45,7 +45,7 @@ menu() {
         *) continue;;
       esac
       currentOption="${@:hovering:1}"
-      echo -e "\e[$(((COLUMNS-${#currentOption})/2))C${currentOption}"
+      echo -e "  [\e[32m*\e[0m] ${currentOption}"
     fi
 
   done
