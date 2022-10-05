0ece3037ff8b88423a2246cee59442536d5f0329
TyraVex <thomas.baruzier@gmail.com>
2022-10-06 00:58:47 +0200

    update

--- rewrite.sh
+++ rewrite.sh
@@ -43,43 +43,61 @@ usage() {
   echo
   echo "Input options :"
   echo
-  echo "  -i, --include {videos|photos|audio}"
-  echo "  -e, --exclude {videos|photos|audio}"
-  echo "      > Include or exclude file types"
-  echo
-  echo "  -r, --recursive"
-  echo "      > Include subfolders"
-  echo "  -o, --overwrite"
-  echo "      > Overwrites already compressed files"
-  echo "  -t, --threads <all|number-of-threads>"
-  echo "      > Number of threads to use"
+#  echo "  -i, --include {videos|photos|audio}"
+#  echo "  -e, --exclude {videos|photos|audio}"
+#  echo "      > Include or exclude file types"
+#  echo "      > Default : include everything"
+#  echo
+#  echo "  -r, --recursive"
+#  echo "      > Include subfolders"
+#  echo "      > Default : true"
+#  echo "  -o, --overwrite"
+#  echo "      > Overwrites already compressed files"
+#  echo "      > Default : false"
+#  echo "  -t, --threads <all|number-of-threads>"
+#  echo "      > Number of threads to use"
+#  echo "      > Default : all"
   echo
   echo "Output options :"
   echo
   echo "  -C, --codec {jpg|jxl|avif|h264|h265|vp9|av1|vvc|mp3|opus} {quality}"
   echo "      > Choose encoding codecs and quality parameters"
-  echo "      > Quality arguments : (values are sorted from best to worse)"
-  echo "        • jpg <q:scale> (2-31) <preset> (placebo-veryfast)"
-  echo "        • jxl"
-  echo "        • avif <min> (0-63) <max> (0-63) <speed> (0-10)"
-  echo "        • h264|h265 <crf> (0-63) <preset> (placebo-veryfast) <opus-bitrate> (256-6)"
-  echo "        • vp9 <crf> (0-63) <cpu-used> (-8 - 8) <opus-bitrate> (256-6)"
-  echo "        • av1 <cq-level> (0-8) <cpu-used> (0-9) <opus-bitrate> (256-6)"
-  echo "        • vvc"
-  echo "        • mp3|opus <bitrate>"
-  echo "      > Example : -C vp9 21 -5 avif 30 mp3 192 opus"
-  echo "  -c, --crop {all|photos|videos} <width>x<height>"
-  echo "      > Crop and zoom to fit whithout distortions"
-  echo "  -m, --max-size {all|photos|videos} <pixels>"
-  echo "      > Set a maximum lenght size in pixels"
-  echo "  -R, --rename {all|photos|videos|audio}"
-  echo "      > Rename the output files to their timestamps"
+#  echo "      > Quality arguments :"
+#  echo "        {quality score/10} {compression efficiency/10} {audio quality/10}"
+#  echo "      > Default : "
+#  echo "      > Quality arguments (for expert users) :"
+#  echo "      > Example : -C vp9 21 -5 avif 30 mp3"
+#  echo "        • jpg <q:scale> (2-31) <preset> (placebo-veryfast)"
+#  echo "          > Default : "
+#  echo "        • jxl (planned for future versions)"
+#  echo "          > Default : "
+#  echo "        • avif <min> (0-63) <max> (0-63) <speed> (0-10)"
+#  echo "          > Default : "
+#  echo "        • h264|h265 <crf> (0-63) <preset> (placebo-veryfast) <opus-bitrate> (256-6)"
+#  echo "          > Default : "
+#  echo "        • vp9 <crf> (0-63) <cpu-used> (-8-8) <opus-bitrate> (256-6)"
+#  echo "          > Default : "
+#  echo "        • av1 <cq-level> (0-8) <cpu-used> (0-9) <opus-bitrate> (256-6)"
+#  echo "          > Default : "
+#  echo "        • vvc <preset> (slower-fast) <bitrate|qc> (n|0-63)"
+#  echo "          > Default : "
+#  echo "        • mp3|opus <bitrate> (256-6)"
+#  echo "          > Default : "
+#  echo "  -c, --crop {all|photos|videos} <width>x<height>"
+#  echo "      > Crop and zoom to fit whithout distortions"
+#  echo "      > Default : "
+#  echo "  -m, --max-size {all|photos|videos} <pixels>"
+#  echo "      > Set a maximum lenght size in pixels"
+#  echo "      > Default : "
+#  echo "  -R, --rename {all|photos|videos|audio}"
+#  echo "      > Rename the output files to their timestamps"
+#  echo "      > Default : "
   echo
   echo "Other"
   echo
-  echo "  -v, --verbose : print more information"
-  echo "  -l, --log {file} : --verbose redirected to a file"
-  echo "  -h, --help : print this menu"
+#  echo "  -v, --verbose : print more information"
+#  echo "  -l, --log {file} : --verbose redirected to a file"
+#  echo "  -h, --help : print this menu"
   echo
   exit
 
@@ -91,20 +109,48 @@ args=($*); [[ -z "${args[0]}" ]] && usage
 for ((i=0; i < "${#args[@]}"; i++)); do
 
   case "${args[i]}" in
-    '--codec'|'-c') ((i++))
-      echo "deal : ${args[i]}"
+    '--codec'|'-C') ((i++))
 
       while [[ "${args[i]:0:1}" != '-' && "${args[i]}" != '' ]]; do
         case "${args[i]}" in
-          'vp9'|'av1') [[ -z "$videoCodec" ]] && videoCodec="${args[i]}" || \
-            echo -e "\e[31mERROR : Please input only one video codec\e[0m";;
-          'jpg'|'avif') [[ -z "$imageCodec" ]] && imageCodec="${args[i]}" || \
+
+          'h264'|'h265'|'vp9'|'av1'|'vvc')
+            if [[ -z "$videoCodec" ]]; then
+              videoCodec="${args[i]}"
+
+              while [[ ! "${args[i]:0:1}" =~ 'h264'|'h265'|'vp9'|'av1'|'vvc'|'jpg'|'avif'|'jxl'|'mp3'|'opus' && "${args[i]}" != '' ]]; do
+                case "$videoCodec" in
+                  'h264'|'h265')
+                    case "${args[i]}" in
+
+
+                    esac
+                  'vp9')
+                    
+                  'av1')
+                    
+                  'vvc')
+                    
+                esac
+                ((i++))
+              done
+              ((i--));;
+
+            else
+              echo -e "\e[31mERROR : Please input only one video codec\e[0m"
+            fi;;
+
+          'jpg'|'avif'|'jxl') [[ -z "$imageCodec" ]] && imageCodec="${args[i]}" || \
             echo -e "\e[31mERROR : Please input only one image codec\e[0m";;
+
           'mp3'|'opus') [[ -z "$audioCodec" ]] && audioCodec="${args[i]}" || \
             echo -e "\e[31mERROR : Please input only one audio codec\e[0m";;
+
           *) echo -e "\e[31mERROR : Bad codec\e[0m"; echo; usage;;
+
         esac
         ((i++))
+
       done
       ((i--));;
 
