91aab623f282fc10cba93e6d1c4eeac8e919e490
TyraVex <thomas.baruzier@gmail.com>
2022-10-08 12:34:48 +0200

    update

--- another-one.sh
+++ another-one.sh
@@ -1,32 +1,52 @@
 #!/bin/bash
 
+config() {
+  :
+}
 
-#=-------=# CONFIG #=-------=#
+defaults() {
+
+  recursive='false'
+  overwrite='false'
+  threads='all'
+
+}
 
 
+debugInfo() {
 
+  echo
+  echo "INPUT OPTIONS :"
+  echo
+  echo "> Input : "
+  echo "> Output : "
+  echo
+  echo "> Targeting : "
+  echo "> Included extentions : "
+  echo "> Excluded extentions : "
+  echo
+  echo "> Recursive : $recursive"
+  echo "> Overwrite : $overwrite"
+  echo "> Threads : $threads"
+  echo
 
-
-
-
-#=--------------------------=#
-
+}
 
 printHelp() {
 
   echo
   echo "USAGE : $0 <input> <output> [arguments]"
   echo
-  echo "INPUT OPTIONS :"
+  echo "Input options :"
   echo
-  echo "  -i, --include {videos|photos|audio}"
-  echo "  -e, --exclude {videos|photos|audio}"
-  echo "      > Include or exclude file types"
+  echo "  -i, --include {all|none|videos|photos|audio|<extention>}"
+  echo "  -e, --exclude {all|none|videos|photos|audio|<extention>}"
+  echo "      > Include or exclude file types or extentions"
   echo "      > Default : include everything"
   echo
   echo "  -r, --recursive"
   echo "      > Include subfolders"
-  echo "      > Default : true"
+  echo "      > Default : false"
   echo "  -o, --overwrite"
   echo "      > Overwrites already compressed files"
   echo "      > Default : false"
@@ -34,7 +54,7 @@ printHelp() {
   echo "      > Number of threads to use"
   echo "      > Default : all"
   echo
-#  echo "Output options :"
+#  echo "Encoding options :"
 #  echo
 #  echo "  -C, --codec {jpg|jxl|avif|h264|h265|vp9|av1|vvc|mp3|opus} {quality}"
 #  echo "      > Choose encoding codecs and quality parameters"
@@ -65,9 +85,15 @@ printHelp() {
 #  echo "  -m, --max-size {all|photos|videos} <pixels>"
 #  echo "      > Set a maximum lenght size in pixels"
 #  echo "      > Default : "
-#  echo "  -R, --rename {all|photos|videos|audio}"
+#  echo
+#  echo "Output options :"
+#  echo
+#  echo "  -R, --rename {all|none|photos|videos|audio}"
 #  echo "      > Rename the output files to their timestamps"
 #  echo "      > Default : "
+#  echo "  -T, --copy-tree"
+#  echo "      > Replicate the input folder hierarachy for output"
+#  echo "      > Default : true"
 #  echo
 #  echo "Other"
 #  echo
@@ -79,41 +105,7 @@ printHelp() {
 
 }
 
-
-argsParsing() {
-
-  args=($*); [[ -z "${args[0]}" ]] && error 'noArg'
-  for ((i=0; i < "${#args[@]}"; i++)); do
-    previousArg="${args[i]}"
-    echo ""
-    case "${args[i]}" in
-      '-i'|'--include'|'-e'|'--exclude') getNextArgs; ieOptions;;
-      '-r'|'--recursive') recursive='true';;
-      '-o'|'--overwrite') overwrite='true';;
-#      '-t'|'--threads') threads="${args[i+1]}";;
-#      *) error 'badArg' "${args[i]}";;
-    esac
-    [ "${args[i+1]}" = '' ] && exit
-  done
-
-}
-
-
-ieOptions() {
-
-  [[ "$previousArg" =~ '-i' ]] && ieVar='include' || ieVar='exclude'
-  for ((i=0; i < "${#nextArgs[@]}"; i++)); do
-    case "${nextArgs[i]}" in
-      'movie'|'movies'|'video'|'videos') echo "Videos : ${ieVar}d";;
-      'image'|'images'|'photo'|'photos') echo "Photos : ${ieVar}d";;
-      'music'|'musics'|'audio'|'audios') echo "Audio files : ${ieVar}d";;
-      '') error 'noParam' "-${ieVar:0:1} or --$ieVar";;
-      *) error 'badParam' "-${ieVar:0:1} or --$ieVar" "${nextArgs[i]}";;
-    esac
-  done
-
-}
-
+# USEFUL FUNCTIONS
 
 getNextArgs() {
 
@@ -128,7 +120,7 @@ getNextArgs() {
 
 error() {
 
-  echo -en '\e[31m\n> ERROR : '
+  echo -en '\e[31m\nERROR : '
   case "$1" in
     'badArg') echo "Unknown argument provided : $2";;
     'noArg') echo "No arguments provided";;
@@ -141,4 +133,75 @@ error() {
 }
 
 
+warn() {
+
+  echo -en '\e[33m\nWARNING : '
+  case "$1" in
+    'maxThreads') echo "Using $2 threads instead of $3 (it's the maximum available)";;
+  esac
+  echo -en '\e[0m'
+
+}
+
+# ARGUMENT RELATED FUNCTIONS
+
+argsParsing() {
+
+  args=($*); [[ -z "${args[0]}" ]] && error 'noArg'
+  for ((i=0; i < "${#args[@]}"; i++)); do
+    previousArg="${args[i]}"
+    case "${args[i]}" in
+      '-i'|'--include'|'-e'|'--exclude') getNextArgs; ieOptions;;
+      '-r'|'--recursive') recursive='true';;
+      '-o'|'--overwrite') overwrite='true';;
+      '-t'|'--threads') ((i++)); threadOption;;
+      '-d') :;;
+      *) error 'badArg' "${args[i]}";;
+    esac
+  done
+
+}
+
+
+ieOptions() {
+
+  [[ "$previousArg" =~ '-i' ]] && ieVar='include' || ieVar='exclude'
+  [[ -z "${nextArgs[@]}" ]] && error 'noParam' "-${ieVar:0:1} or --$ieVar"
+  for ((j=0; j < "${#nextArgs[@]}"; j++)); do
+    case "${nextArgs[j]}" in
+      'image'*|'photo'*|'picture'*)
+        echo "Photos : ${ieVar}d";;
+      'movie'*|'video'*)
+        echo "Videos : ${ieVar}d";;
+      'music'*|'audio'*)
+        echo "Audio files : ${ieVar}d";;
+      'all'|'everything')
+        echo "Everything : ${ieVar}d";;
+      'none'|'nothing')
+        echo "Nothing : ${ieVar}d";;
+      *) error 'badParam' "-${ieVar:0:1} or --$ieVar" "${nextArgs[j]}";;
+    esac
+  done
+
+}
+
+
+threadOption() {
+
+  [[ -z "${args[i]}" ]] && error 'noParam' "-t or --threads"
+  availableThreads="$(($(cat /proc/cpuinfo | grep -Po 'processor[^0-9]+\K[0-9]+$' | tail -n 1)+1))"
+  case "${args[i]}" in
+    'all'|'max'|'everything')
+      threads="$availableThreads";;
+     [1-9]|[0-9][0-9]|[0-9][0-9][0-9])
+      (( "${args[i]}" > "$availableThreads" )) && \
+        warn 'maxThreads' "$availableThreads" "${args[i]}" || \
+      threads="$((${args[i]}))";;
+    *) error 'badParam' '-t or --threads' "${args[i]}";;
+  esac
+
+}
+
+defaults
 argsParsing "$@"
+debugInfo
