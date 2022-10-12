df0c273139b7b7e960f5f8ebc31e6a41ca4d91e2
TyraVex <thomas.baruzier@gmail.com>
2022-10-12 17:05:44 +0200

    update

---
+++ experimental/bruh.sh
@@ -0,0 +1,224 @@
+#!/bin/bash
+
+
+# INITIALISATION
+
+getConfig() {
+
+  input=''
+  output=''
+
+  videos='true'
+  photos='true'
+  audios='true'
+
+  includedExtentions=''
+  excludedExtentions=''
+
+  recursive='false'
+  overwrite='false'
+  threads='all'
+
+  verbose='false'
+  logging='false'
+  loglevel='info'
+
+}
+
+processArgs() {
+
+  args=($@); [ "$args" = '' ] && error 'noArg'
+  for ((i=0; i < "${#args[@]}"; i++)); do
+    for ((j=0; j < "${#optionNames[@]}"; j++)); do
+      [[ "${optionIDs[j]}" = "${args[i]}" || "${optionNames[j]}" = "${args[i]}" ]] \
+      && launchOption "${optionNames[j]:2}Option"
+    done
+  done
+#  [ "$verbose" = 'true' ] && echo debug
+
+}
+
+launchOption() {
+
+  unset nextArgs
+  while [[ "${args[i+1]:0:1}" != '-' && -n "${args[i+1]}" ]]; do
+    ((i++))
+    nextArgs+=("${args[i]}")
+  done
+  $1 "${nextArgs[@]}"
+
+}
+
+optionBuilder() {
+
+  readarray -t options < "$0"
+  i=0; while [[ "${options[i-2]}" != '# OPTIONS' ]]; do ((i++)); done
+  for ((; i <= "${#options[@]}"; i++)); do
+    if [[ "${options[i]}" =~ ^[a-zA-Z]+$ ]]; then
+      optionName="--${options[i],,}"
+      optionNames+=("$optionName")
+      optionID="${optionName:1:2}"
+      [[ "${optionIDs[@]}" =~ "$optionID" ]] && optionID="${optionID^}"
+      [[ ! "${optionIDs[@]}" =~ "$optionID" ]] && optionIDs+=("$optionID") || echo "No more ids available"
+    elif [[ "${options[i]}" =~ ';;'$ ]]; then
+      optionCases+="${options[i]} "
+    elif [[ -n "${options[i]}" ]]; then
+      optionTasks+="${options[i]};"
+    elif [[ -n "$optionName" ]]; then
+      source <(echo "${optionName:2}Option"'() { optionArgs=(${*}); [[ -z "${optionArgs[@]}" ]] && error "noParam" '"'$optionID or $optionName'"'; '"$optionTasks"' for ((index=0; index < "${#optionArgs[@]}"; index++)); do case "${optionArgs[index]}" in '"$optionCases"' esac; done; }')
+#      echo "${optionName:2}Option"'() { optionArgs=(${*}); [[ -z "${optionArgs[@]}" ]] && error "noParam" '"'$optionID or $optionName'"'; '"$optionTasks"' for ((index=0; index < "${#optionArgs[@]}"; index++)); do case "${optionArgs[index]}" in '"$optionCases"' esac; done; }'
+      unset optionName optionID optionCases optionTasks
+    fi
+  done
+
+}
+
+# DEBUGGING
+
+printHelp() {
+
+  echo
+  echo "USAGE : $0 <input> <output> [arguments]"
+  echo
+  echo "Input options :"
+  echo
+  echo "  -i, --include {all|none|videos|photos|audios|<extention>}"
+  echo "  -e, --exclude {all|none|videos|photos|audios|<extention>}"
+  echo "      > Include or exclude file types or extentions"
+  echo "      > Default : include everything"
+  echo
+  echo "  -r, --recursive"
+  echo "      > Include subfolders"
+  echo "      > Default : false"
+  echo "  -o, --overwrite"
+  echo "      > Overwrites already compressed files"
+  echo "      > Default : false"
+  echo
+#  echo "Encoding options :"
+#  echo
+#  echo "  -C, --codec {jpg|jxl|avif|h264|h265|vp9|av1|vvc|mp3|opus} {quality}"
+#  echo "      > Choose encoding codecs and quality parameters"
+#  echo "      > Quality arguments :"
+#  echo "        {quality score/10} {compression efficiency/10} {audio quality/10}"
+#  echo "      > Default : "
+#  echo "      > Quality arguments (for expert users) :"
+#  echo "      > Example : -C vp9 21 -5 avif 30 mp3"
+#  echo "        • jpg <q:scale> (2-31) <preset> (placebo-veryfast)"
+#  echo "          > Default : "
+#  echo "        • jxl (planned for future versions)"
+#  echo "          > Default : "
+#  echo "        • avif <min> (0-63) <max> (0-63) <speed> (0-10)"
+#  echo "          > Default : "
+#  echo "        • h264|h265 <crf> (0-63) <preset> (placebo-veryfast) <opus-bitrate> (256-6)"
+#  echo "          > Default : "
+#  echo "        • vp9 <crf> (0-63) <cpu-used> (-8-8) <opus-bitrate> (256-6)"
+#  echo "          > Default : "
+#  echo "        • av1 <cq-level> (0-8) <cpu-used> (0-9) <opus-bitrate> (256-6)"
+#  echo "          > Default : "
+#  echo "        • vvc <preset> (slower-fast) <bitrate|qc> (n|0-63)"
+#  echo "          > Default : "
+#  echo "        • mp3|opus <bitrate> (256-6)"
+#  echo "          > Default : "
+#  echo "  -c, --crop {all|photos|videos} <width>x<height>"
+#  echo "      > Crop and zoom to fit whithout distortions"
+#  echo "      > Default : "
+#  echo "  -m, --max-size {all|photos|videos} <pixels>"
+#  echo "      > Set a maximum lenght size in pixels"
+#  echo "      > Default : "
+#  echo
+#  echo "Output options :"
+#  echo
+#  echo "  -R, --rename {all|none|photos|videos|audios}"
+#  echo "      > Rename the output files to their timestamps"
+#  echo "      > Default : "
+#  echo "  -T, --copy-tree"
+#  echo "      > Replicate the input folder hierarachy for output"
+#  echo "      > Default : true"
+#  echo
+  echo "Other options :"
+  echo
+  echo "  -t, --threads <all|number-of-threads>"
+  echo "      > Number of threads to use"
+  echo "      > Default : all"
+  echo
+  echo "  -v, --verbose : print more information"
+  echo "  -l, --log {file} : --verbose redirected to a file"
+  echo "  -L, --loglevel <0|1|2|info|warning|error>"
+  echo "  -h, --help : print this menu"
+  echo
+  exit
+
+}
+
+error() {
+
+  echo -en '\e[31m\nERROR : '
+  case "$1" in
+    'badArg') echo "Unknown argument provided : $2";;
+    'noArg') echo "No arguments provided";;
+    'badParam') echo "Wrong parameter provided for argument $2 : $3";;
+    'noParam') echo "No parameter provided for argument $2";;
+    'badPath') echo "Non-existing path provided : $2";;
+  esac
+  echo -en '\e[0m'
+  printHelp
+
+}
+
+warn() {
+
+  [ "$loglevel" = 'error' ] && return
+  echo -en '\e[33m\nWARNING : '
+  case "$1" in
+    'maxThreads') echo "Using $2 threads instead of $3 (it's the maximum available)";;
+  esac
+  echo -e '\e[0m'
+
+}
+
+info() {
+
+  [[ "$loglevel" = 'error' || "$loglevel" = 'warning' ]] && return
+  echo -en '\nINFO : '
+  case "$1" in
+    'noFile') echo "No filename provided for $2 : Using $3";;
+  esac
+  echo
+
+}
+
+# MAIN PROGRAM
+
+optionBuilder
+getConfig
+processArgs "$@"
+exit
+
+# OPTIONS
+
+THREADS
+availableThreads="$(($(cat /proc/cpuinfo | grep -Po 'processor[^0-9]+\K[0-9]+$' | tail -n 1)+1))"
+all|max|everything) threads="$availableThreads";;
+[1-9]|[0-9][0-9]|[0-9][0-9][0-9]) (( "$1" > "$availableThreads" )) && warn "maxThreads" "$availableThreads" "$1" || threads="$(($1))";;
+*) error 'badParam' '-t or --threads' "$1";;
+
+INCLUDE
+image*|photo*|picture*|pic*) photos='true';;
+movie*|video*|vid*) videos='true';;
+music*|audio*) audios='true';;
+all|everything) photos='true'; videos='true'; audios='true';;
+none|nothing) photos='false'; videos='false'; audios='false';;
+*) extention="${1/\./}"; extention="${extention,,}"; includedExtentions+="$extention";;
+
+EXCLUDE
+image*|photo*|picture*|pic*) photos='false';;
+movie*|video*|vid*) videos='false';;
+music*|audio*) audios='false';;
+all|everything) photos='false'; videos='false'; audios='false';;
+none|nothing) photos='true'; videos='true'; audios='true';;
+*) extention="${1/\./}"; extention="${extention,,}"; includedExtentions+="$extention";;
+
+VERBOSE
+''|y|yes|enable|true) verbose='true';;
+n|no|disable|false) verbose='false';;
+*) error 'badParam' '-v or --verbose' "$1";;
---
+++ experimental/rewrite.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+
+
+#=-----=# CONFIG #=-----=#
+
+input=
+output=
+codec=()
+
+rename=(n) # {y|n|photos|videos|audio}
+crop=(n) # {y|n|photos|video} {}
+
+overwrite=
+recursive=
+verbose=
+logging=
+threads=
+crf=
+
+include=() # {all|photos|videos|audio}
+exclude=() # {photos|videos|audio}
+
+musicBitrate='128'
+audioBitrate='64'
+
+av1Quality='2'
+vp9Quality='-5'
+avifQuality='0'
+
+avifMaxQuality='63'
+avifMinQuality='0'
+
+maxSize='1280'
+doMaxSize='y'
+
+#=-=# END OF CONFIG #=-=#
+
+
+# help menu
+usage() {
+
+  echo "USAGE : $0 <input> <output> [arguments] "
+  echo
+  echo "Input options :"
+  echo
+#  echo "  -i, --include {videos|photos|audio}"
+#  echo "  -e, --exclude {videos|photos|audio}"
+#  echo "      > Include or exclude file types"
+#  echo "      > Default : include everything"
+#  echo
+#  echo "  -r, --recursive"
+#  echo "      > Include subfolders"
+#  echo "      > Default : true"
+#  echo "  -o, --overwrite"
+#  echo "      > Overwrites already compressed files"
+#  echo "      > Default : false"
+#  echo "  -t, --threads <all|number-of-threads>"
+#  echo "      > Number of threads to use"
+#  echo "      > Default : all"
+  echo
+  echo "Output options :"
+  echo
+  echo "  -C, --codec {jpg|jxl|avif|h264|h265|vp9|av1|vvc|mp3|opus} {quality}"
+  echo "      > Choose encoding codecs and quality parameters"
+#  echo "      > Quality arguments :"
+#  echo "        {quality score/10} {compression efficiency/10} {audio quality/10}"
+#  echo "      > Default : "
+#  echo "      > Quality arguments (for expert users) :"
+#  echo "      > Example : -C vp9 21 -5 avif 30 mp3"
+#  echo "        • jpg <q:scale> (2-31) <preset> (placebo-veryfast)"
+#  echo "          > Default : "
+#  echo "        • jxl (planned for future versions)"
+#  echo "          > Default : "
+#  echo "        • avif <min> (0-63) <max> (0-63) <speed> (0-10)"
+#  echo "          > Default : "
+#  echo "        • h264|h265 <crf> (0-63) <preset> (placebo-veryfast) <opus-bitrate> (256-6)"
+#  echo "          > Default : "
+#  echo "        • vp9 <crf> (0-63) <cpu-used> (-8-8) <opus-bitrate> (256-6)"
+#  echo "          > Default : "
+#  echo "        • av1 <cq-level> (0-8) <cpu-used> (0-9) <opus-bitrate> (256-6)"
+#  echo "          > Default : "
+#  echo "        • vvc <preset> (slower-fast) <bitrate|qc> (n|0-63)"
+#  echo "          > Default : "
+#  echo "        • mp3|opus <bitrate> (256-6)"
+#  echo "          > Default : "
+#  echo "  -c, --crop {all|photos|videos} <width>x<height>"
+#  echo "      > Crop and zoom to fit whithout distortions"
+#  echo "      > Default : "
+#  echo "  -m, --max-size {all|photos|videos} <pixels>"
+#  echo "      > Set a maximum lenght size in pixels"
+#  echo "      > Default : "
+#  echo "  -R, --rename {all|photos|videos|audio}"
+#  echo "      > Rename the output files to their timestamps"
+#  echo "      > Default : "
+  echo
+  echo "Other"
+  echo
+#  echo "  -v, --verbose : print more information"
+#  echo "  -l, --log {file} : --verbose redirected to a file"
+#  echo "  -h, --help : print this menu"
+  echo
+  exit
+
+}
+
+# argument processing
+echo
+args=($*); [[ -z "${args[0]}" ]] && usage
+for ((i=0; i < "${#args[@]}"; i++)); do
+
+  case "${args[i]}" in
+    '--codec'|'-C') ((i++))
+
+      while [[ "${args[i]:0:1}" != '-' && "${args[i]}" != '' ]]; do
+        case "${args[i]}" in
+
+          'h264'|'h265'|'vp9'|'av1'|'vvc')
+            if [[ -z "$videoCodec" ]]; then
+              videoCodec="${args[i]}"
+
+              while [[ ! "${args[i]:0:1}" =~ 'h264'|'h265'|'vp9'|'av1'|'vvc'|'jpg'|'avif'|'jxl'|'mp3'|'opus' && "${args[i]}" != '' ]]; do
+                case "$videoCodec" in
+                  'h264'|'h265')
+                    if [[ -z "$h264-5Crf" ]]; then
+                      if (( "${args[i]}" > 0 && "${args[i]}" < 64 )); then
+                        h264-5Crf="${args[i]}"
+                      else
+                        echo -e "\e[31mERROR : Bad CRF (${args[i]}). This value should be between 0 and 63"
+                    else
+
+                    esac
+                  'vp9')
+                    
+                  'av1')
+                    
+                  'vvc')
+                    
+                esac
+                ((i++))
+              done
+              ((i--));;
+
+            else
+              echo -e "\e[31mERROR : Please input only one video codec\e[0m"
+            fi;;
+
+          'jpg'|'avif'|'jxl') [[ -z "$imageCodec" ]] && imageCodec="${args[i]}" || \
+            echo -e "\e[31mERROR : Please input only one image codec\e[0m";;
+
+          'mp3'|'opus') [[ -z "$audioCodec" ]] && audioCodec="${args[i]}" || \
+            echo -e "\e[31mERROR : Please input only one audio codec\e[0m";;
+
+          *) echo -e "\e[31mERROR : Bad codec\e[0m"; echo; usage;;
+
+        esac
+        ((i++))
+
+      done
+      ((i--));;
+
+    '--recursive'|'-r') recursivity='true';;
+    '--verbose'|'-v') verbose='true';;
+    '--help'|'-h') usage;;
+
+    -*)echo -e "\e[31mERROR : Invalid argument : '${args[i]}'\e[0m"
+       [[ "${args[i]:0:1}" != '-' ]] && ((i++)); echo; usage;;
+
+    *) if [[ -z "$input" ]]; then
+         input="$(readlink -f ${args[i]})"
+         if [[ ! -d "$input" && ! -f "$input" ]]; then
+           echo -e "\e[31mERROR : Input path isn't a file or directory : '$input'\e[0m" && echo && exit
+         fi
+       elif [[ -z "$output" ]]; then
+         output="$(readlink -f ${args[i]})"
+         if [[ ! -d "$output" ]]; then
+           echo -e "\e[31mERROR : Output path isn't a directory : '${output}'\e[0m\n"
+           read -ren1 -p "Do you want to create it ? (default=n) : " makeOutputDirectory
+           echo
+           [[ "$makeOutputDirectory" = 'y' ]] && mkdir -p "$output" || unset output
+         fi
+       fi;;
+
+  esac
+
+done
+
+[[ -z "$input" ]] && echo -e "\e[31mERROR : Input and output paths aren't set\e[0m\n" && exit
+[[ -z "$output" ]] && echo -e "\e[31mERROR : Output path isn't set\e[0m\n" && exit
+
+# verbose information
+if [[ "$verbose" == 'true' ]]; then
+  [[ -z "$input" ]] && input='not set'
+  [[ -z "$output" ]] && output='not set'
+  [[ -z "$recursivity" ]] && recursivity='false'
+  [[ -z "$videoCodec" ]] && videoCodec='av1'
+  [[ -z "$imageCodec" ]] && imageCodec='avif'
+  [[ -z "$audioCodec" ]] && audioCodec='opus'
+  echo -en '\e[36m'
+  echo 'DEBBUGGING :'
+  echo
+  echo "Input : $input"
+  echo "Output : $output"
+  echo "Recursivity : $recursivity"
+  echo "Video codec : $videoCodec"
+  echo "Image codec : $imageCodec"
+  echo "Audio codec : $audioCodec"
+  echo -e '\e[0m'
+fi
+
+echo 'Starting...'
+echo
